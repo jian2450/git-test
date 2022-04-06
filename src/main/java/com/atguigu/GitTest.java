ee8f419579f12d6e28bdc54ee6e48b6239751f1d
james <jian2450@qq.com>
2022-04-06 23:09:59 +0800

    second commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author jian
+ * @create 2022-04-06 23:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
